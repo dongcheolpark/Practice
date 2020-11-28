c2b1e36641c4f03f79c3e632bf07fb0e4bfba2ea
dongcheolpark <head02325@gmail.com>
2020-11-28 11:42:41 +0900

    monogame

---
+++ c#/monogame/test1/Game1.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace test1
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1() //생성자
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()//초기설정
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()//콘텐츠 불러오기
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)//입력 업데이트
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)//그리기
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- cpp/cpptest/cpptest/cpptest.cpp
+++ cpp/cpptest/cpptest/cpptest.cpp
@@ -1,6 +1,15 @@
-﻿#include <iostream>
+﻿#include <cstdio>
+
+int fact(int a);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    printf("%d", fact(3));
+    return 0;
+}
+
+int fact(int a) {
+
+    if (!a) return 1;
+    return fact(a-1)*a; 
 }
