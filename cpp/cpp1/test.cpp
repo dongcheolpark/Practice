f701f2dd0b87721c68480d71b3cedf3d05499b3d
parkdongcheol <head02325@gmail.com>
2021-03-25 00:34:17 +0900

    a

--- cpp/cpp1/test.cpp
+++ cpp/cpp1/test.cpp
@@ -1,33 +1,21 @@
 #include <cstdio>
 
-#define MAX_SIZE 1000000
-
-int arr[MAX_SIZE];
-
-void swap(int a,int b) {
-
-}
-
-void partition(int left, int right) {
-
-}
-
-void sort() {
-
-}
-
 int main()
 {
-    int n, n2;
-    int arr[1000];
-    scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", arr + i);
+    int n = 0,cnt[10000];
+    scanf("%d",&n);
+    for(int i = 0;i<10000;i++) {
+        cnt[i] = 0;
     }
-    
-    for (int i = 0; i < n; i++)
-    {
-        printf("%d\n", arr[i]);
+    for(int i = 0; i<n;i++) {
+        int tmp = 0;
+        scanf("%d",&tmp);
+        cnt[tmp]++;
+    }
+    for(int i = 0;i<n;i++) {
+        for(int j = 0;j<cnt[i];j++) {
+            printf("%d ",i);
+        }
     }
     return 0;
 }
