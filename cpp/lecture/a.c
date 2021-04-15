b103e52a4664fd2dd13395080bec7c3c9c8ee6d6
dongcheolpark <head02325@gmail.com>
2021-04-15 21:37:39 +0900

    a

--- cpp/lecture/a.c
+++ cpp/lecture/a.c
@@ -1,11 +1,8 @@
-#include <limits.h>
-#include <float.h>
 #include <stdio.h>
-
-
-int main() {
-    printf("%Le\n",LDBL_MIN);
-    printf("%Le\n",LDBL_MAX);
-    printf("%d\n",LDBL_DIG);
+int main(void){
+    int num;
+    printf("3자리 정수를 입력하세요 : ");
+    scanf("%d",num);
+    printf("백의 자리 숫자 : %d\n 일의 자리 숫자 : %d",num/100,num%10);
     return 0;
 }
