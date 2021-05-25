0d87aafca8594eac966812ef8d55951c4740ac2c
dongcheolpark <head02325@gmail.com>
2021-05-25 19:53:06 +0900

    a

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -21,40 +21,40 @@ bool arr[100000];
 
 int main() {
     int n,m;
-            printf("%d ",10);
-    scanf("%d %d",&n,&m);
-            printf("%d ",20);
+    scanf("%d%d",&n,&m);
     q.push(pair1(n,0));
-            printf("%d ",30);
     arr[n] = 1;
-            printf("%d ",40);
-    for(int i = 1;i<10;i++) {
+    for(int i = 1;;i++) {
         int l = q.size();
         for(int j =0;j<l;j++) {
             pair1 tmp = q.front();
-            printf("%d ",tmp.x);
+            //printf("%d ",tmp.x);
             q.pop();
             if(!arr[tmp.x+1]) {
                 if(tmp.x+1 == m) {
-                    printf("%d ",i+1);
+                    printf("%d ",i);
+                    return 0;
                 }
                 arr[tmp.x+1] = 1;
                 q.push(pair1(tmp.x+1,i));
             } 
-            if(!arr[tmp.x-1]) {
+            if(tmp.x>=0&&!arr[tmp.x-1]) {
                 if(tmp.x-1 == m) {
-                    printf("%d ",i+1);
+                    printf("%d ",i);
+                    return 0;
                 }
                 arr[tmp.x-1] = 1;
                 q.push(pair1(tmp.x-1,i));
             } 
             if(!arr[tmp.x*2]) {
                 if(tmp.x*2 == m) {
-                    printf("%d ",i+1);
+                    printf("%d ",i);
+                    return 0;
                 }
                 arr[tmp.x*2] = 1;
                 q.push(pair1(tmp.x*2,i));
             } 
         }
+        //puts("");
     }
 }
