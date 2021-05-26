826e6926677b25d2bb1314ddf1779f8fc1e73bc7
dongcheolpark <head02325@gmail.com>
2021-05-26 14:33:10 +0900

    a

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -17,11 +17,15 @@ struct pair1 {
 };
 
 queue<pair1> q;
-bool arr[100000];
+bool arr[100001];
 
 int main() {
     int n,m;
     scanf("%d%d",&n,&m);
+    if(n == m) {
+        printf("0");
+        return 0;
+    }
     q.push(pair1(n,0));
     arr[n] = 1;
     for(int i = 1;;i++) {
@@ -30,7 +34,7 @@ int main() {
             pair1 tmp = q.front();
             //printf("%d ",tmp.x);
             q.pop();
-            if(!arr[tmp.x+1]) {
+            if(tmp.x+1 <= 100000&&!arr[tmp.x+1]) {
                 if(tmp.x+1 == m) {
                     printf("%d ",i);
                     return 0;
@@ -38,7 +42,7 @@ int main() {
                 arr[tmp.x+1] = 1;
                 q.push(pair1(tmp.x+1,i));
             } 
-            if(tmp.x>=0&&!arr[tmp.x-1]) {
+            if(tmp.x-1>=0&&!arr[tmp.x-1]) {
                 if(tmp.x-1 == m) {
                     printf("%d ",i);
                     return 0;
@@ -46,7 +50,7 @@ int main() {
                 arr[tmp.x-1] = 1;
                 q.push(pair1(tmp.x-1,i));
             } 
-            if(!arr[tmp.x*2]) {
+            if(tmp.x*2 <= 100000&&!arr[tmp.x*2]) {
                 if(tmp.x*2 == m) {
                     printf("%d ",i);
                     return 0;
