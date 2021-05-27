a7f1a36b7c32fbf22ac80057f096f18d9c9989d3
dongcheolpark <head02325@soongsil.ac.kr>
2021-05-27 19:19:12 +0900

    a

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -7,58 +7,29 @@
 
 using namespace std;
 
-struct pair1 {
-    int x;
-    int cnt;
-    pair1(int x,int cnt) {
-        this->x = x;
-        this->cnt = cnt;
+struct time
+{
+    int begin;
+    int end;
+    bool operator<(time &b) {
+        if(this->end == b.end) {
+            return this->begin < b.begin;
+        }
+        else return this->end < b.end;
     }
 };
 
-queue<pair1> q;
-bool arr[100001];
+vector<time> arr;
 
 int main() {
-    int n,m;
-    scanf("%d%d",&n,&m);
-    if(n == m) {
-        printf("0");
-        return 0;
+    int n;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++) {
+        time tmp = NULL; 
+        scanf("%d %d",tmp.begin,tmp.end);
+        arr.push_back(tmp);
     }
-    q.push(pair1(n,0));
-    arr[n] = 1;
-    for(int i = 1;;i++) {
-        int l = q.size();
-        for(int j =0;j<l;j++) {
-            pair1 tmp = q.front();
-            //printf("%d ",tmp.x);
-            q.pop();
-            if(tmp.x+1 <= 100000&&!arr[tmp.x+1]) {
-                if(tmp.x+1 == m) {
-                    printf("%d ",i);
-                    return 0;
-                }
-                arr[tmp.x+1] = 1;
-                q.push(pair1(tmp.x+1,i));
-            } 
-            if(tmp.x-1>=0&&!arr[tmp.x-1]) {
-                if(tmp.x-1 == m) {
-                    printf("%d ",i);
-                    return 0;
-                }
-                arr[tmp.x-1] = 1;
-                q.push(pair1(tmp.x-1,i));
-            } 
-            if(tmp.x*2 <= 100000&&!arr[tmp.x*2]) {
-                if(tmp.x*2 == m) {
-                    printf("%d ",i);
-                    return 0;
-                }
-                arr[tmp.x*2] = 1;
-                q.push(pair1(tmp.x*2,i));
-            } 
-        }
-        //puts("");
-    }
+
+    sort(arr.begin(),arr.end());
+
 }
