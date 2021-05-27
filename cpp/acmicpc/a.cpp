2a7b1ec95909c481e4c21666cf63e057cb25ae81
dongcheolpark <head02325@soongsil.ac.kr>
2021-05-27 19:45:11 +0900

    A

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -7,33 +7,54 @@
 
 using namespace std;
 
-bool arr[100][100];
-bool chk[100] = {0,};
-int n,m,res;
+bool arr[128][128];
+int n;
 
-void func(int a) {
-    if(chk[a]) {
+int check_res(int num,int x,int y) {
+    bool chk = arr[x][y];
+    for(int i = x;i<num+x;i++) {
+        for(int j = y;j<num+y;j++) {
+            if(chk != arr[i][j]) return 0;
+        }
+    }
+    if(chk == 0) {
+        return 1;
+    }
+    if(chk == 1) {
+        return 2;
+    }
+}
+
+int res1 = 0,res2=0;
+
+void func(int num,int x,int y) {
+    int tmp = check_res(num,x,y);
+    if(num == 1) {
+        if(arr[x][y] == 1) res1++;
+        else res2++;
+    }
+    if(tmp == 1) {
+        res1++;
         return;
     }
-    chk[a] = 1;
-    res++;
-    for(int i = 0;i<n;i++) {
-        if(arr[a][i]) {
-            func(i);
+    if(tmp == 2) {
+        res2++;
+        return;
+    }
+    for(int i = 0;i<2;i++) {
+        for(int j = 0;j<2;j++) {
+            func(num/2,x+num*i,y+num*j);
         }
     }
 }
 
-
 int main() {
-    scanf("%d %d",&n,&m);
-    for(int i =0;i<m;i++) {
-        int a,b;
-        scanf("%d %d",&a,&b);
-        arr[a][b] = 1;
-        arr[b][a] = 1;
+    scanf("%d",&n);
+    for(int i = 0;i<n;i++) {
+        for(int j = 0;j<n;j++) {
+            scanf("%d",arr+i);
+        }
     }
-    func(0);
-    printf("%d",res);
-
+    func(n,0,0);
+    printf("%d\n%d",res1,res2);
 }
