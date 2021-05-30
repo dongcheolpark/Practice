d70a25eee6a30e7de3285d26285bbde50fdb1de7
dongcheolpark <head02325@soongsil.ac.kr>
2021-05-30 20:09:34 +0900

    a

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -7,42 +7,67 @@
 
 using namespace std;
 
+class max_hip {
+private:
+    int * data;
+    int size;
+    void swap(int & a,int & b) {
+        int tmp = a;
+        a = b;
+        b = tmp;
+    }
+public:
+    max_hip(int n) {
+        data = new int[n];
+        size = 0;
+    }
+    void push(int n) {
+        data[size] = n;
+        size++;
+        for(int i = (size-1)/2;(i-1)/2>=0;i = (i-1)/2) {
+            if(data[(i-1)/2] < data[i]) {
+                swap(data[(i-1)/2],data[i]);
+            }
+            else break;
+        }
+    }
+    int top() {
+        if(size == 0) return 0;
+        return data[0]; 
+    }
+    void pop() {
+        if(size == 0) return;
+        data[0] = data[size];
+        data[size] = 0;
+        size--;
+        int n = 1; 
+        while(n < size) {
+            for(int i = n;i<n+2;i++) {
+                if(data[(i-1)/2] < data[i]) {
+                    swap(data[(i-1)/2],data[i]);
+                    n = i;
+                }
+                break;
+            }
+            n *= 2+1;
+        }
+    }
 
+};
 
 int main() {
-    int n,m;
-    scanf("%d\n",&n);
+    max_hip * hip;
+    int n;
+    scanf("%d",&n);
+    hip = new max_hip(n);
     while(n--) {
-        scanf("%d\n",&m);
-        int cnt = 0;
-        priority_queue<int, vector<int>, greater<int>> pq_asc;
-        priority_queue<int, vector<int>> pq_des;
-        while(m--) {
-            char ch;
-            int tmp;
-            scanf("%c %d\n",&ch,&tmp);
-            if(cnt == 0) {
-                while(!pq_asc.empty()) pq_asc.pop();
-                while(!pq_des.empty()) pq_des.pop();
-            }
-            if(ch == 'I') {
-                pq_des.push(tmp);
-                pq_asc.push(tmp);
-                cnt++;
-            }
-            else {
-                if(!cnt) continue;
-                if(tmp == -1) {
-                    pq_des.pop();
-                    cnt--;
-                }
-                else {
-                    pq_asc.pop();
-                    cnt--;
-                }
-            }
+        int tmp;
+        scanf("%d",&tmp);
+        if(tmp == 0) {
+            printf("%d\n",hip->top());
+            hip->pop();
         }
-        if(cnt == 0) printf("EMPTY\n");
-        else printf("%d %d\n",pq_des.top(),pq_asc.top());
+        else hip->push(tmp);
     }
+    
 }
