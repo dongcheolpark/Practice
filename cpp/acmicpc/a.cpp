3293083ad093862af7e5d3f67ec1913cceb9045f
dongcheolpark <head02325@soongsil.ac.kr>
2021-05-30 16:54:19 +0900

    a

--- cpp/acmicpc/a.cpp
+++ cpp/acmicpc/a.cpp
@@ -7,54 +7,42 @@
 
 using namespace std;
 
-bool arr[128][128];
-int n;
 
-int check_res(int num,int x,int y) {
-    bool chk = arr[x][y];
-    for(int i = x;i<num+x;i++) {
-        for(int j = y;j<num+y;j++) {
-            if(chk != arr[i][j]) return 0;
-        }
-    }
-    if(chk == 0) {
-        return 1;
-    }
-    if(chk == 1) {
-        return 2;
-    }
-}
-
-int res1 = 0,res2=0;
-
-void func(int num,int x,int y) {
-    int tmp = check_res(num,x,y);
-    if(num == 1) {
-        if(arr[x][y] == 1) res1++;
-        else res2++;
-    }
-    if(tmp == 1) {
-        res1++;
-        return;
-    }
-    if(tmp == 2) {
-        res2++;
-        return;
-    }
-    for(int i = 0;i<2;i++) {
-        for(int j = 0;j<2;j++) {
-            func(num/2,x+num*i,y+num*j);
-        }
-    }
-}
 
 int main() {
-    scanf("%d",&n);
-    for(int i = 0;i<n;i++) {
-        for(int j = 0;j<n;j++) {
-            scanf("%d",arr+i);
+    int n,m;
+    scanf("%d\n",&n);
+    while(n--) {
+        scanf("%d\n",&m);
+        int cnt = 0;
+        priority_queue<int, vector<int>, greater<int>> pq_asc;
+        priority_queue<int, vector<int>> pq_des;
+        while(m--) {
+            char ch;
+            int tmp;
+            scanf("%c %d\n",&ch,&tmp);
+            if(cnt == 0) {
+                while(!pq_asc.empty()) pq_asc.pop();
+                while(!pq_des.empty()) pq_des.pop();
+            }
+            if(ch == 'I') {
+                pq_des.push(tmp);
+                pq_asc.push(tmp);
+                cnt++;
+            }
+            else {
+                if(!cnt) continue;
+                if(tmp == -1) {
+                    pq_des.pop();
+                    cnt--;
+                }
+                else {
+                    pq_asc.pop();
+                    cnt--;
+                }
+            }
         }
+        if(cnt == 0) printf("EMPTY\n");
+        else printf("%d %d\n",pq_des.top(),pq_asc.top());
     }
-    func(n,0,0);
-    printf("%d\n%d",res1,res2);
 }
