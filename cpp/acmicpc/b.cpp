e95e9ad89410aaad833a4e13c5efae7ae99026db
dongcheolpark <head02325@gmail.com>
2021-07-14 00:06:06 +0900

    a

--- cpp/acmicpc/b.cpp
+++ cpp/acmicpc/b.cpp
@@ -1,15 +1,4 @@
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cstring>
-#include <cmath>
-#include <algorithm>
-#include <utility>
-#include <queue>
-#include <map>
-#include <vector>
-
-using namespace std;
 
 int main() {
 	
