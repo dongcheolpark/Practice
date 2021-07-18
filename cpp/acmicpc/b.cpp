14383280109c1b71f3f19d6a1a72ba355041b5ca
dongcheolpark <head02325@gmail.com>
2021-07-18 16:47:03 +0900

    a

--- cpp/acmicpc/b.cpp
+++ cpp/acmicpc/b.cpp
@@ -1,24 +1,9 @@
-#include <cstdio>
+#include <iostream>
 #include <string>
-#include <algorithm>
-#include <map>
 #include <vector>
 
 using namespace std;
-vector<int> v;
 
 int main() {
-	int n;
-	scanf("%d",&n);
-	for(int i = 0;i<n;i++) {
-		int tmp;
-		scanf("%d",&tmp);
-		v.push_back(tmp);
-	}
-	sort(v.begin(),v.end());
-	int res;
-	for(int i = 0;i<n;i++) {
-		res += (n-i)*v[i];
-	}
-	printf("%d",res);
+
 }
--- cpp/acmicpc/c.cpp
+++ cpp/acmicpc/c.cpp
@@ -4,13 +4,5 @@
 using namespace std;
 
 int main() {
-    vector<int> v({1,2,3,4,5}); // 리스트를 활용한 벡터 초기화
-
-    for(auto i = v.begin(); i < v.end(); i++) {
-        cout<< *i << endl; // 반복자를 이용한 순차탐색
-    }
-
-    for(auto i = v.rbegin(); i < v.rend(); i++) {
-        cout<< *i << endl; // 역반복자를 이용한 역순차탐색
-    }
+    printf("%d",(3>>2)%2);
 }
