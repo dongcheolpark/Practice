807ff58cc6e699844fbb9f40dea693436d210f73
dongcheolpark <head02325@soongsil.ac.kr>
2021-07-19 13:50:10 +0900

    a

--- cpp/acmicpc/b.cpp
+++ cpp/acmicpc/b.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
-#include <string.h>
+#include <algorithm>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
+int two(int x) {
+	int res = 0;
+	for(;!(x%2);x/=2) {
+		res++;
+	}
+	return res;
+}
+
+int five(int x) {
+	int res = 0;
+	for(;!(x%5);x/=5) {
+		res++;
+	}
+	return res;
+}
+
 int main() {
-	
+	int n;
+	scanf("%d",&n);
+	int a = 0,b = 0;
+	for(int i = 1;i<=n;i++) {
+		a += two(i);
+		b += five(i);
+	}
+	printf("%d",min(a,b));
 }
