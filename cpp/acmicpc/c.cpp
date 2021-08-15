a4ec88d627a82b776e21f4b8712747d9a69b5e2c
dongcheolpark <head02325@gmail.com>
2021-08-15 22:43:41 +0900

    a

--- cpp/acmicpc/c.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-int main() {
-    printf("%d",(3>>2)%2);
-}
---
+++ cpp/acmicpc/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	int t;
+	priority_queue<pair<int,int>,vector<pair<int,int> >,greater<int>> pq;
+	while(t--) {
+		
+	}	
+}
