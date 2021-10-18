7a8250f30913f181134a726063658532db3838f4
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-10-18 15:50:53 +0900

    a

--- cpp/midterm/a.c
+++ cpp/midterm/a.c
@@ -1,57 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int ToInteger(char ch) {
-    switch(ch) {
-        case 'r':
-            return 1;
-        case 's':
-            return 0;
-        case 'p':
-            return 2;
-        case '9':
-            printf("다음에 또 만나요.\n");
-            exit(0);
-        default :
-            printf("잘못 입력하셨습니다. 다시 입력해주세요\n");
-            return -1;
-    }
-}
-
-char* ToHanguel(int num) {
-    switch(num) {
-        case 0:
-            return "가위";
-        case 1:
-            return "바위";
-        case 2:
-            return "보";
-    }
-}
-
-char* compare_num(int a, int b) {
-    if(a == b) { //비김
-        return "당신이 비겼습니다.";
-    }
-    else if((a == 0 && b == 2) || (a == 2 && b == 1) || (a == 1 && b == 0)) { //이김
-        return "당신이 이겼습니다.";
-    }
-    else return "당신이 졌습니다.";//짐 
-}
 
 int main() {
-    char ch = 0;
-    printf("가위(s) 바위(r) 보(p) 게임을 시작합니다.(게임종료 : 9)\n");
-    while(1){
-        printf("가위 바위 보를 선택하세요. : ");
-        ch = getchar();
-        while(getchar() != '\n');
-        int num = ToInteger(ch);
-        if(num == -1) continue;
-        srand((unsigned int)time(NULL));
-        int random_num = rand()%3;
-        printf("당신은 %s를 냈고 컴퓨터는 %s를 냈습니다.\n",ToHanguel(num),ToHanguel(random_num));
-        puts(compare_num(num,random_num));
-    }
+	int i,*p = &i;
+	i = (int)p;
+	printf("%d",i);
 }
--- cpp/test/a.c
+++ cpp/test/a.c
@@ -1,65 +1,45 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-#define N 10
+enum cost_value {dollar,won};
 
-typedef struct student
+typedef union cost_va {
+	double dollar;
+	int won;
+}const_va;
+
+typedef struct product
 {
 	char * name;
-	char * email;
-	char * phone;
-}student;
-
-student data[10];
-
-void print_student(int i) {
-	printf("name : %s\nemail : %s\nphone : %s\n\n",data[i].name,data[i].email,data[i].phone);
-}
-
-void search_name(char * name) {
-	for(int i = 0;i<N;i++) {
-		if(strcmp(name,data[i].name) == 0) {
-			print_student(i);
-		}
-	}
-}
-void search_all() {
-	for(int i = 0;i<N;i++) {
-		print_student(i);
-	}
-
-}
+	const_va cost;
+	enum cost_value cost_v;
+}product;
 
 
 int main() {
-	for(int i = 0;i<N;i++) {
-		char * tmp;
-		tmp = (char *)malloc(sizeof(char)*41);
-		scanf("%s",tmp);
-		data[i].name = tmp; 
-		tmp = (char *)malloc(sizeof(char)*41);
-		scanf("%s",tmp);
-		data[i].email = tmp; 
-		tmp = (char *)malloc(sizeof(char)*41);
-		scanf("%s",tmp);
-		data[i].phone = tmp; 
+	const product product_list[6] = {
+		{"pmp",{.won = 600000,},won},
+		{"mp3",{.won = 153000,},won},
+		{"smartphone",{.dollar = 520.40},dollar},
+		{"tv",{.dollar = 430.20},dollar},
+		{"navigator",{.won = 350000,},won},
+		{"laptop",{.dollar = 1950.00},dollar}
+	};
+	int cost,exchange;
+	puts("보유금액과 환율을 입력하세요.");
+	printf("보유금액 : ");
+	scanf("%d",&cost);
+	printf("환율 : ");
+	scanf("%d",&exchange);
+	for(int i = 0;i<6;i++) {
+		int won;
+		if(product_list[i].cost_v == dollar) {
+			won = (int)(product_list[i].cost.dollar * exchange);
+		}
+		else {
+			won = product_list[i].cost.won;
+		}
+		if(won < cost) {
+			printf("%s %d원\n",product_list[i].name,won);
+		}
 	}
-
-	puts("1. 이름검색, 2.전체검색");
-	int a;
-	scanf("%d",&a);
-	if(a == 1) {
-		char tmp[40];
-		printf("이름 :");
-		scanf("%s",tmp);
-		search_name(tmp);
-	}
-	else if(a== 2) {
-		search_all();
-	}
-	else {
-		puts("잘못입력하셨습니다.");
-	}
-	return 0;
 }
