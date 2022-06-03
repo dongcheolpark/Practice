dacf42c0aca0075243fd9be70b2eb4dc56afe445
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-06-03 11:37:15 +0000

    asm

---
+++ cpp/assembly/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 4
+
+/*int main() {
+	int arr[N][N],res[N][N];
+	for(int i = 1;i<=N;i++) {
+		for(int j = 1;j<=N;j++) {
+			arr[i][j] = i*j;
+		}
+	}
+	return 0;
+}*/
+int main() {
+	int * arr = malloc(sizeof(int)*10);
+}
